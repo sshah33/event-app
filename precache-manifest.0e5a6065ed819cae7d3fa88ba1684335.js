517fc4afcc32f075fa82c80aced3a2decaf0d113
Shreyans Shah <sshah33@gmu.edu>
2018-10-30 13:25:41 -0700

    Updates

--- precache-manifest.0e5a6065ed819cae7d3fa88ba1684335.js
+++ precache-manifest.0e5a6065ed819cae7d3fa88ba1684335.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/event-app/static/js/runtime~main.5e580696.js"
   },
   {
-    "revision": "8b0929bb848274dfd593",
-    "url": "/event-app/static/js/main.8b0929bb.chunk.js"
+    "revision": "2e0607d9dbbae7ef8fe2",
+    "url": "/event-app/static/js/main.2e0607d9.chunk.js"
   },
   {
     "revision": "d25ba8d82700f1078d63",
     "url": "/event-app/static/js/1.d25ba8d8.chunk.js"
   },
   {
-    "revision": "8b0929bb848274dfd593",
-    "url": "/event-app/static/css/main.7a95c8db.chunk.css"
+    "revision": "2e0607d9dbbae7ef8fe2",
+    "url": "/event-app/static/css/main.7f7c91ca.chunk.css"
   },
   {
-    "revision": "ba388c471272e7fcf232b1732ed48b00",
+    "revision": "84f84bd33168624b7b5beabc65578110",
     "url": "/event-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/event-app/precache-manifest.b2d820b1e7c62d48cb88cb6fd0fefdf4.js"
+  "/event-app/precache-manifest.0e5a6065ed819cae7d3fa88ba1684335.js"
 );
 
 workbox.clientsClaim();
