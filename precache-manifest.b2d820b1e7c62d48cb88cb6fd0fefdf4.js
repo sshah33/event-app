2e09787c282ba6a8eb4ddcccaf822ed0fefe71c4
Shreyans Shah <sshah33@gmu.edu>
2018-10-30 13:04:41 -0700

    Updates

---
+++ precache-manifest.b2d820b1e7c62d48cb88cb6fd0fefdf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e580696a6d16882529b",
+    "url": "/event-app/static/js/runtime~main.5e580696.js"
+  },
+  {
+    "revision": "8b0929bb848274dfd593",
+    "url": "/event-app/static/js/main.8b0929bb.chunk.js"
+  },
+  {
+    "revision": "d25ba8d82700f1078d63",
+    "url": "/event-app/static/js/1.d25ba8d8.chunk.js"
+  },
+  {
+    "revision": "8b0929bb848274dfd593",
+    "url": "/event-app/static/css/main.7a95c8db.chunk.css"
+  },
+  {
+    "revision": "ba388c471272e7fcf232b1732ed48b00",
+    "url": "/event-app/index.html"
+  }
+];
